7f2bd7c3e62a4dcd86651c50fe1b9d0118d0150d
Enguyen1115 <105744661+Enguyen1115@users.noreply.github.com>
2022-06-29 12:51:21 -0600

    commit

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -4,32 +4,35 @@ import About from './component/about';
 import Home from './component/home';
 import Contact from './component/contact';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-      return (
-          <Router>
-              <div className="App">
-                  <ul className="App-header">
-                      <li>
-                          <Link to="/">Home</Link>
-                      </li>
-                      <li>
-                          <Link to="/about">About Us</Link>
-                      </li>
-                      <li>
-                          <Link to="/contact">Contact Us</Link>
-                      </li>
-                  </ul>
-                  <Routes>
-                      <Route exact path='/' element={< Home />}></Route>
-                      <Route exact path='/about' element={< About />}></Route>
-                      <Route exact path='/contact' element={< Contact />}></Route>
-                  </Routes>
-              </div>
-          </Router>
-      );
+    return (
+      //     <Router>
+      //         <div className="App">
+      //             <ul className="App-header">
+      //                 <li>
+      //                     <Link to="/">Home</Link>
+      //                 </li>
+      //                 <li>
+      //                     <Link to="/about">About Us</Link>
+      //                 </li>
+      //                 <li>
+      //                     <Link to="/contact">Contact Us</Link>
+      //                 </li>
+      //             </ul>
+      //             <Routes>
+      //                 <Route exact path='/' element={< Home />}></Route>
+      //                 <Route exact path='/about' element={< About />}></Route>
+      //                 <Route exact path='/contact' element={< Contact />}></Route>
+      //             </Routes>
+      //         </div>
+      //     </Router>
+      <div>
+        
+      </div>
+    );
   }
 }
 
